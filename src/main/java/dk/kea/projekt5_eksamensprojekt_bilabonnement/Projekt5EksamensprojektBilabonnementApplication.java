6e6d8283228740728b0876cef4df4b44172a5aab
Martin Poulsen <mapo0004@stud.kea.dk>
2024-05-03 09:05:54 +0200

    add git

---
+++ src/main/java/dk/kea/projekt5_eksamensprojekt_bilabonnement/Projekt5EksamensprojektBilabonnementApplication.java
@@ -0,0 +1,13 @@
+package dk.kea.projekt5_eksamensprojekt_bilabonnement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projekt5EksamensprojektBilabonnementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Projekt5EksamensprojektBilabonnementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dk/kea/projekt5_eksamensprojekt_bilabonnement/Projekt5EksamensprojektBilabonnementApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.projekt5_eksamensprojekt_bilabonnement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projekt5EksamensprojektBilabonnementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
