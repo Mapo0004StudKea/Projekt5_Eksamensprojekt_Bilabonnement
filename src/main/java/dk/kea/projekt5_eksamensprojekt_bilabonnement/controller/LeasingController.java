ae41b9c9a7ffd651ba66b7a4c2165a3a50706ae1
Viggo2102 <viktor.rasmussen@hotmail.com>
2024-05-09 14:17:20 +0200

    Leasingcontroller og html.

--- src/main/java/dk/kea/projekt5_eksamensprojekt_bilabonnement/controller/LeasingController.java
+++ src/main/java/dk/kea/projekt5_eksamensprojekt_bilabonnement/controller/LeasingController.java
@@ -18,6 +18,7 @@ public class LeasingController {
         return "watchLeasingAgreements";
     }
 
+
     @GetMapping("/makeNewLeasing")
     public String makeNewLeasing(){
         return "makeNewLeasing";
@@ -25,18 +26,17 @@ public class LeasingController {
 
     @PostMapping("/makeNewLeasing")
     public String newLeasingMade(
-            @RequestParam("employe") String employee_name,
-            @RequestParam("mothlyPrice") double monthly_price,
-            @RequestParam("customer") String customer_name,
-            @RequestParam("start_leasing") LocalDate start_leasing,
-            @RequestParam("end_leasing") LocalDate end_leasing,
-            @RequestParam("is_unlimited") boolean is_unlimited,
-            @RequestParam("is_limited") boolean is_limited,
-            @RequestParam("car_id") int car_id ) {
+            @RequestParam("Pris") double monthly_price,
+            @RequestParam("startdato") LocalDate start_leasing,
+            @RequestParam("slutdato") LocalDate end_leasing,
+            @RequestParam("navn") String customer_name,
+            @RequestParam("Ubegrænset") boolean is_unlimited,
+            @RequestParam("limited") boolean is_limited,
+            @RequestParam("carId") int car_id,
+            @RequestParam("ansat") String employee_name){
 
         LeasingModel leasingModel =new LeasingModel(monthly_price,start_leasing,end_leasing,customer_name,is_unlimited,is_limited,car_id,employee_name);
         leasingRepository.leasingNewContrakt(leasingModel);
-
 
         return "redirect:/watchLeasingAgreements";
     }
