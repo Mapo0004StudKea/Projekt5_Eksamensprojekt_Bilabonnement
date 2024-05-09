a0f913a381924c2e52216fcbc36e28ea41291473
ViggoBeck <vibe0002@stud.kea.dk>
2024-05-09 13:24:47 +0200

    CRUD Metoder leasingREP

--- src/main/java/dk/kea/projekt5_eksamensprojekt_bilabonnement/repository/LeasingRepository.java
+++ src/main/java/dk/kea/projekt5_eksamensprojekt_bilabonnement/repository/LeasingRepository.java
@@ -1,19 +1,40 @@
 package dk.kea.projekt5_eksamensprojekt_bilabonnement.repository;
 
 import dk.kea.projekt5_eksamensprojekt_bilabonnement.model.LeasingModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class LeasingRepository {
+    @Autowired
     JdbcTemplate jdbcTemplate;
 
+    private LeasingModel leasingModel;
 
 //jeg laver en metode som oprette en LeasingMetode
-    public void leasingNewContrakt(LeasingModel leasingModel){
-        final String INSERT_leasingcontract_SQL = "INSERT INTO leasingcontract (leasingPrice, startLeasing, endLeasing, customerName, isLimited, isUnlimited) VALUES (?,?,?,?,?,?)";
-        jdbcTemplate.update(INSERT_leasingcontract_SQL, leasingModel.getMonthly_price(),leasingModel.getStart_leasing(),leasingModel.getEnd_leasing(),leasingModel.getCustomer_name(),leasingModel.isIs_limited(),leasingModel.isIs_unlimited());
+    public void createLeasingContract(LeasingModel leasing){
+        final String INSERT_leasingcontract_SQL = "INSERT INTO leasingcontract (employee_name, monthly_price, customerName, startLeasing, endLeasing, isUnlimited, isLimited, car_id) VALUES (?,?,?,?,?,?,?,?)";
+        jdbcTemplate.update(INSERT_leasingcontract_SQL, leasing.getEmployee_name(), leasing.getMonthly_price(), leasing.getCustomer_name(), leasing.getStart_leasing(), leasing.getEnd_leasing(),leasing.isIs_unlimited(),leasing.isIs_limited(), leasing.getCar_id());
+    }
+
+    public void deleteLeasing(int id){
+        String DELETE_LEASING_SQL = "DELETE FROM leasingcontract WHERE id = ?";
+        jdbcTemplate.update(DELETE_LEASING_SQL, id);
+    }
+    public void updateLeasing(LeasingModel leasing){
+        String UPDATE_LEASING_SQL = "UPDATE leasingcontract SET employee_name = ?, monthly_price = ?, customer_name = ?, start_leasing = ?, end_leasing = ?, is_unlimited = ?, is_limited = ?, car_id = ? WHERE id = ?";
+        jdbcTemplate.update(UPDATE_LEASING_SQL, leasing.getEmployee_name(), leasing.getMonthly_price(), leasing.getCustomer_name(), leasing.getStart_leasing(), leasing.getEnd_leasing(),leasing.isIs_unlimited(),leasing.isIs_limited(), leasing.getCar_id());
+    }
+
+    public LeasingModel findById(int id){
+        String FIND_LEASING_SQL ="SELECT * FROM leasingcontract WHERE id = ?";
+        return jdbcTemplate.queryForObject(FIND_LEASING_SQL, new Object[]{id}, new BeanPropertyRowMapper<>(LeasingModel.class));
 
     }
+
+
+
 
 }
