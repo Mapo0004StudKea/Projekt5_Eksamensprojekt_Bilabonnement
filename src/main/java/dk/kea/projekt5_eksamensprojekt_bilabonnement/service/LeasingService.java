dde6cc6d13af6ff51d36b887d983c3db400aa209
ViggoBeck <vibe0002@stud.kea.dk>
2024-05-09 14:07:46 +0200

    double price

---
+++ src/main/java/dk/kea/projekt5_eksamensprojekt_bilabonnement/service/LeasingService.java
@@ -0,0 +1,20 @@
+package dk.kea.projekt5_eksamensprojekt_bilabonnement.service;
+
+import dk.kea.projekt5_eksamensprojekt_bilabonnement.repository.LeasingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LeasingService {
+
+    @Autowired
+    private LeasingRepository leasingRepository;
+
+    public double calculateLeasingPrice (boolean is_limited, boolean is_unlimited, double monthly_price){
+        double totalPrice = 0;
+        totalPrice += monthly_price;
+
+        return totalPrice;
+    }
+
+}
