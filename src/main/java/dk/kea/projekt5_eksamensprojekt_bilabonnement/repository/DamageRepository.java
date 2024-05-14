c6f9fecfb4a37a5a025af1280eaabb0e7cc06a2d
ViggoBeck <vibe0002@stud.kea.dk>
2024-05-14 09:59:44 +0200

    crud metoder til DamageRepository

--- src/main/java/dk/kea/projekt5_eksamensprojekt_bilabonnement/repository/DamageRepository.java
+++ src/main/java/dk/kea/projekt5_eksamensprojekt_bilabonnement/repository/DamageRepository.java
@@ -1,10 +1,38 @@
 package dk.kea.projekt5_eksamensprojekt_bilabonnement.repository;
 
+import dk.kea.projekt5_eksamensprojekt_bilabonnement.model.DamageModel;
+import dk.kea.projekt5_eksamensprojekt_bilabonnement.model.LeasingModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
 public class DamageRepository {
 
-    public void createNewDamage(){
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
+    private DamageModel damageModel;
+
+    public void createNewDamage(DamageModel damage){
+        final String INSERT_DAMAGE_SQL = "INSERT INTO damages (damage_name, damage_price, damage_description, damageReport_id) VALUES (?, ?, ?, ?)";
+        jdbcTemplate.update(INSERT_DAMAGE_SQL, damage.getDamage_name(), damage.getDamage_price(), damage.getDamage_description(), damage.getDamageReport_id());
     }
-
+    public void deleteDamage(int id){
+        String DELETE_DAMAGE_SQL = "DELETE FROM damages WHERE id = ?";
+        jdbcTemplate.update(DELETE_DAMAGE_SQL, id);
+    }
+    public DamageModel findDamageById(int id){
+        String GET_DAMAGE_SQL = "SELECT * FROM damages WHERE id = ?";
+        return jdbcTemplate.queryForObject(GET_DAMAGE_SQL, new BeanPropertyRowMapper<>(DamageModel.class), id);
+    }
+    public List<DamageModel> findAllDamage(){
+        String GET_ALL_DAMAGE_SQL = "SELECT * FROM damages";
+        List<DamageModel> damageList = jdbcTemplate.query(GET_ALL_DAMAGE_SQL, new BeanPropertyRowMapper<>(DamageModel.class));
+        return damageList;
+    }
 }
 
