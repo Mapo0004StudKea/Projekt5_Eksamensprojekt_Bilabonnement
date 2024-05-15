5c80b21b34d40cf9cb0473b6db1f1242139e6953
Viggo2102 <viktor.rasmussen@hotmail.com>
2024-05-15 12:13:56 +0200

    Forbundet fra damgereport og til at lave skader med id. Mangler at lave tabel i watchDamageReport html så man kan se oprettelsen af skader.

--- src/main/java/dk/kea/projekt5_eksamensprojekt_bilabonnement/controller/DamageReportController.java
+++ src/main/java/dk/kea/projekt5_eksamensprojekt_bilabonnement/controller/DamageReportController.java
@@ -30,39 +30,40 @@ public class DamageReportController {
     @GetMapping("/DamageReportSite")
     public String viewDamageReport(Model model) {
         List<DamageReportModel> allDamageReports = damageReportRepository.getFullListOfReport();
-        model.addAttribute("damageReport",allDamageReports);
+        model.addAttribute("damageReport", allDamageReports);
         return "DamageReportSite";
     }
 
     @GetMapping("/CreateNewReportEntry")
-    public String CreateNewReportEntry(){
-        return "CreateNewReportEntry";}
+    public String CreateNewReportEntry() {
+        return "CreateNewReportEntry";
+    }
 
     @PostMapping("/CreateNewReportEntry")
     public String MakeNewReportEntry(@RequestParam("name") String name,
                                      @RequestParam("description") String description,
                                      @RequestParam("employee") String employee,
-                                     @RequestParam("car_id")int car_id)
-    {
-    DamageReportModel damageReportModel = new DamageReportModel(name,description,employee,car_id);
-    damageReportRepository.createNewDamageReport(damageReportModel);
-    return "redirect:/DamageReportSite";
+                                     @RequestParam("car_id") int car_id) {
+        DamageReportModel damageReportModel = new DamageReportModel(name, description, employee, car_id);
+        damageReportRepository.createNewDamageReport(damageReportModel);
+        return "redirect:/DamageReportSite";
     }
 
     @GetMapping("/UpdateReport/{id}")
-    public String updateReportByID(@PathVariable( "id") int reportID, Model model){
+    public String updateReportByID(@PathVariable("id") int reportID, Model model) {
         DamageReportModel damageReportModel = damageReportRepository.getDamageReportInDatabaseByID(reportID);
         model.addAttribute("updateReport", damageReportModel);
-        return "UpdateReport";}
+        return "UpdateReport";
+    }
 
 
     @PostMapping("/UpdateReport")
-    public String updateReportByID( @RequestParam("id") int id,
-                                    @RequestParam("report_name") String report_name,
-                                     @RequestParam("report_description") String report_description,
-                                     @RequestParam("report_employee_name") String report_employee_name,
-                                     @RequestParam("car_id") int car_id){
-        DamageReportModel damageReportModel = new DamageReportModel(id, report_name,report_description,report_employee_name,car_id);
+    public String updateReportByID(@RequestParam("id") int id,
+                                   @RequestParam("report_name") String report_name,
+                                   @RequestParam("report_description") String report_description,
+                                   @RequestParam("report_employee_name") String report_employee_name,
+                                   @RequestParam("car_id") int car_id) {
+        DamageReportModel damageReportModel = new DamageReportModel(id, report_name, report_description, report_employee_name, car_id);
         damageReportRepository.UpdateDamageReportEntryInDatabase(damageReportModel);
         return "redirect:DamageReportSite";
     }
@@ -78,4 +79,10 @@ public class DamageReportController {
         }
     }
 
+    @GetMapping("/watchDamageReport/{id}")
+    public String watchDamageReport(@PathVariable("id") int id, Model model) {
+        DamageReportModel damageReportModel = damageReportRepository.getDamageReportInDatabaseByID(id);
+        model.addAttribute("watchdamage", damageReportModel);
+        return "watchDamageReport";
+    }
 }
