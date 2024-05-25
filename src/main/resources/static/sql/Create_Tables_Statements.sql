104c568e1c322e92ac08d15a68b651199e81a2db
Martin Poulsen <mapo0004@stud.kea.dk>
2024-05-25 10:15:43 +0200

    lavet lidt rettelse i fejlhåndteringen så beskeden ser lidt pænere ud og har rettet vore sqæ scripts så de passer med det der står i mysql

--- src/main/resources/static/sql/Create_Tables_Statements.sql
+++ src/main/resources/static/sql/Create_Tables_Statements.sql
@@ -1,35 +1,34 @@
-CREATE TABLE `car` (
-                       `id` int NOT NULL AUTO_INCREMENT,
-                       `car_picture` varchar(255) DEFAULT NULL,
-                       `car_full_name` varchar(255) DEFAULT NULL,
-                       `car_name` varchar(50) DEFAULT NULL,
-                       `car_model` varchar(50) DEFAULT NULL,
-                       `car_serialnr` varchar(50) NOT NULL,
-                       `car_number` int NOT NULL,
-                       `car_year` int DEFAULT NULL,
-                       `monthly_price` double DEFAULT NULL,
-                       `is_leased` int DEFAULT NULL,
-                       `car_description` varchar(255) DEFAULT NULL,
-                       PRIMARY KEY (`id`)
+CREATE TABLE IF NOT EXISTS car (
+    id              int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    car_picture     varchar(255) DEFAULT NULL,
+    car_full_name   varchar(255) DEFAULT NULL,
+    car_name        varchar(50) DEFAULT NULL,
+    car_model       varchar(50) DEFAULT NULL,
+    car_serialnr    varchar(50) NOT NULL,
+    car_number      varchar(50) NOT NULL,
+    car_year        int DEFAULT NULL,
+    monthly_price   double DEFAULT NULL,
+    is_leased       int DEFAULT NULL,
+    car_description varchar(255) DEFAULT NULL,
 )
 
 CREATE TABLE IF NOT EXISTS leasingcontract (
-                                               id	INT 	AUTO_INCREMENT	NOT NULL	PRIMARY KEY,
-                                               employee_name VARCHAR(100)	NOT NULL,
+    id	            INT 	AUTO_INCREMENT	NOT NULL	PRIMARY KEY,
+    employee_name   VARCHAR(100)	NOT NULL,
     monthly_price	DOUBLE,
     customer_name 	VARCHAR(100)	NOT NULL,
     start_leasing	DATE	NOT NULL,
     end_leasing		DATE	NOT NULL,
     is_unlimited	INT,
     is_limited		INT,
-    car_id	INT,
+    car_id	        INT,
     FOREIGN KEY (car_id) REFERENCES car(id) ON DELETE CASCADE
     );
 
 
 CREATE TABLE IF NOT EXISTS damages (
-                                       id	INT 	AUTO_INCREMENT	NOT NULL	PRIMARY KEY,
-                                       damage_name	VARCHAR(100) NOT NULL,
+    id	            INT 	AUTO_INCREMENT	NOT NULL	PRIMARY KEY,
+    damage_name	    VARCHAR(100) NOT NULL,
     damage_price	DOUBLE	NOT NULL,
     damage_description		VARCHAR(200),
     damagereport_id	INT,
@@ -37,9 +36,12 @@ CREATE TABLE IF NOT EXISTS damages (
     );
 
 CREATE TABLE IF NOT EXISTS damagereport (
-                                            id	INT 	AUTO_INCREMENT	NOT NULL	PRIMARY KEY,
-                                            report_name	VARCHAR(50) NOT NULL,
-    car_id	INT,
+    id	            INT 	AUTO_INCREMENT	NOT NULL	PRIMARY KEY,
+    report_name	    VARCHAR(50) NOT NULL,
+    report_description VARCHAR(255),
+    report_employee_name VARCHAR(100),
+    report_Damage_Date DATE,
+    car_id	         INT,
     FOREIGN KEY (car_id) REFERENCES car(id) ON DELETE CASCADE
     );
 
